d0fcaa7775d317fe9e9d729eb21f1bca94988078
bakak17 <bakak17@email.arizona.edu>
2021-04-06 11:21:33 -0700

    updating syntax for lightkurve

--- lightcurve_download.py
+++ lightcurve_download.py
@@ -1,19 +1,19 @@
 # coding: utf-8
-from lightkurve import search_lightcurvefile
+import lightkurve as lk
 import matplotlib.pyplot as plt
 def lightcurve_22():
     import lightkurve
     target = "K2-22"
-    K222 = search_lightcurvefile(target, mission='K2', campaign=1).download().PDCSAP_FLUX.normalize().flatten(window_length=101)
+    K222 = lk.search_lightcurve(target, mission='K2', campaign=1).download().PDCSAP_FLUX.normalize().flatten(window_length=101)
     K222.plot()
-    K222.to_fits('K2d22curve.fits')
+    K222.to_fits('K2d22curve.fits', overwrite = True)
 
 
 def lightcurve_1520():
     import lightkurve
     target = "Kepler-1520"
-    lcKep1520 = search_lightcurvefile(target, quarter=1).download().PDCSAP_FLUX.normalize().flatten(window_length=101)
+    lcKep1520 = lk.search_lightcurve(target, quarter=1).download().PDCSAP_FLUX.normalize().flatten(window_length=101)
     for q in range(2, 18):
-        lcKep1520 = lcKep1520.append(search_lightcurvefile(target, quarter=q).download().PDCSAP_FLUX.normalize().flatten(window_length=101))
+        lcKep1520 = lcKep1520.append(lk.search_lightcurve(target, quarter=q).download().PDCSAP_FLUX.normalize().flatten(window_length=101))
     lcKep1520.plot()
-    lcKep1520.to_fits('Kep1520curve.fits')
+    lcKep1520.to_fits('Kep1520curve.fits', overwrite = True)
